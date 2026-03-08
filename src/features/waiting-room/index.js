72869d66ec0caeb9f0fc19a5c7f0ac265d02ce7f
PeterNCTL <nguyencongthienloc10a3@gmail.com>
2026-03-09 05:48:22 +0700

    Quick fix: remove unused import

--- src/features/waiting-room/index.js
+++ src/features/waiting-room/index.js
@@ -1,5 +1,4 @@
 // Waiting room components
-export { default as MediaControls } from "./components/MediaControls"
 export { default as ParticipantList } from "./components/ParticipantList"
 export { default as VideoPreview } from "./components/VideoPreview"
 export { default as WaitingScreen } from "./components/WaitingScreen"
